461a51f71440a4d3ecc5c6bd9df7a6024bac1b1b
Teemu Rauhala <teemu.t.rauhala@gmail.com>
2018-08-08 16:58:56 +0300

    lisää settiä

---
+++ ope_6.1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  8 16:42:08 2018
+
+@author: olli
+"""
+
+a = int(input('Valitse luku: '))
+b = int(input('Valitse toinen luku: '))
+summa = a + b
+print(summa)
---
+++ ope_6.3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  8 16:47:52 2018
+
+@author: olli
+"""
+
+vuosi = 365
+tunnit = 365 * 24
+minuutit = tunnit * 60
+sekunnit = minuutit * 60
+print(sekunnit)

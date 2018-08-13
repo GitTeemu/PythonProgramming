295d93b13b6a9604757f6e82daee6fd8f6501459
Teemu Rauhala <teemu.t.rauhala@gmail.com>
2018-08-13 16:08:49 +0300

    monday mnday part 2

---
+++ ope_10.1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 13 15:22:48 2018
+
+@author: olli
+"""
+
+luku = 1
+while luku < 101:
+    print(luku)
+    luku = luku + 1
+    
+    
+    
---
+++ ope_10.2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 13 15:27:13 2018
+
+@author: olli
+"""
+
+luku = 100
+while luku >= 0:
+    print(luku)
+    luku = luku - 1
+    

fbbef99164f57816c14fb989bcbca27abf222941
Teemu Rauhala <teemu.t.rauhala@gmail.com>
2018-08-20 13:21:24 +0300

    pyythonia

---
+++ ope_12.2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 20 12:16:54 2018
+
+@author: olli
+"""
+
+luvut = [5, 1, 3, 4, 2]
+for luku in reversed(luvut):
+    print(luku)
+    
+    

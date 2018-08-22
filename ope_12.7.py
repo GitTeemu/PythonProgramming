0ce5127478d51ce883b4cd4a66e8faf81b21e6a2
Teemu Rauhala <teemu.t.rauhala@gmail.com>
2018-08-22 15:31:16 +0300

    listoja

---
+++ ope_12.7.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 21 18:06:45 2018
+
+@author: olli
+"""
+
+luvut = [7, 2, 4, 8]
+luku = int(input('Anna luku: '))
+if luku in luvut:
+    print('luku on listassa')
+else:
+    print('luku ei ole listassa')
+    
+    
+
+
